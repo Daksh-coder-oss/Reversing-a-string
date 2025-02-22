8c4af63589c40da04702a14b0c003a2754433574
Daksh-coder-oss <vv0039190@gmail.com>
2025-02-22 09:52:25 +0000

    Add files via upload

---
+++ Reversing_string.py
@@ -0,0 +1,12 @@
+class Reverse:
+    def __init__(self,s):
+        self.s=""
+    def word(self):
+        s=input("Please enter the word that is to be reversed")
+    def reverse(self):
+        print(reverse.s(self))
+obj=Reverse
+obj.word()
+obj.reverse()
+    
+    
